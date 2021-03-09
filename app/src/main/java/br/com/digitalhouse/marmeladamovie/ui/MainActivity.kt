59fa90d2cd63c139e28a7a4f63a6fbdd4bb3d942
williamcansizabot <william.zabot@eldorado.org.br>
2021-03-08 22:06:27 -0300

    gradle modified

--- app/src/main/java/br/com/digitalhouse/marmeladamovie/ui/MainActivity.kt
+++ app/src/main/java/br/com/digitalhouse/marmeladamovie/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.digitalhouse.marmeladamovie
+package br.com.digitalhouse.marmeladamovie.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.digitalhouse.marmeladamovie.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
