109d18f87d5f02c4de932dabc29cca6212fa800e
williamzabot <williamzabot123@gmail.com>
2021-12-16 23:08:07 -0300

    exclusão login

--- app/src/main/java/br/com/digitalhouse/marmeladamovie/presenter/features/splash/SplashActivity.kt
+++ app/src/main/java/br/com/digitalhouse/marmeladamovie/presenter/features/splash/SplashActivity.kt
@@ -7,7 +7,6 @@ import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import br.com.digitalhouse.marmeladamovie.R
 import br.com.digitalhouse.marmeladamovie.presenter.features.home.HomeActivity
-import br.com.digitalhouse.marmeladamovie.presenter.features.loginfeats.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
 
 class SplashActivity : AppCompatActivity() {
@@ -16,12 +15,8 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         val auth = FirebaseAuth.getInstance()
-        val logged = auth.currentUser != null
-        val intent = if (logged) {
-            Intent(this, HomeActivity::class.java)
-        } else {
-            Intent(this, LoginActivity::class.java)
-        }
+        Intent(this, HomeActivity::class.java)
+
 
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(intent)
